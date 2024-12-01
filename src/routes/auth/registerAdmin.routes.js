4d5a016a2f03bbae0e387e510c12d563a541360b
VictorAdmin <victorescalona2006@gmail.com>
2024-12-01 13:28:58 -0400

    Agregue seguridad al registrar un administrador, ahora se necesita ingresar directamente a la base ded atos para agregar el primer administrador, y a partir de alli, se podra agregar un admin a traves del endpoint especifico para ello

--- src/routes/auth/registerAdmin.routes.js
+++ src/routes/auth/registerAdmin.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { registerAdmin } from "../../controller/auth/registerAdmin.controller.js";
+import { authenticateToken } from "../../middlewares/verifyJWT.js";
 
 export const router = Router();
 
-router.post("/register", registerAdmin)
+router.post("/register", authenticateToken, registerAdmin)
 
